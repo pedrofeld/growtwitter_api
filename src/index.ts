5635f13107e5fc090da4c7bb8c68d9aef3f0aee6
Pedro Feld <pedro.gael.feld@gmail.com>
2025-12-19 13:57:10 -0300

    docs: including commentary on the CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+/* 
+    It would be necessary to configure CORS to accept requests only from the front-end domain, because it is a browser restriction It does not affect Postman or other API testing tools.
+    Exemple:
+    app.use(cors({
+        origin: 'https://my-front-end.com'
+    }));
+*/
+
 const userRepository = new UserRepository();
 const tweetRepository = new TweetRepository();
 const likeRepository = new LikeRepository();
