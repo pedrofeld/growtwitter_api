c76f7d75b2395e133ea4cdac5272f4b4d17d4d45
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-26 19:33:52 -0300

    fix: correcting indentation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -68,7 +68,10 @@ export class UserController {
         try {
             const { id } = req.params;
             if (!id) {
-                return res.status(400).json({ ok: false, message: "ID is required" });
+                return res.status(400).json({ 
+                    ok: false, 
+                    message: "ID is required" 
+                });
             }
             const user = await this.service.update(id, req.body);
             res.status(200).json({ 
@@ -90,7 +93,10 @@ export class UserController {
         try {
             const { id } = req.params;
             if (!id) {
-                return res.status(400).json({ ok: false, message: "ID is required" });
+                return res.status(400).json({ 
+                    ok: false, 
+                    message: "ID is required" 
+                });
             }
             const user = await this.service.delete(id);
             res.status(200).json({ 
